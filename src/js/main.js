061b205ef02b9376bd420b188d7ad31bd249064c
Aleksandr Piatopal <piatopal.alex@gmail.com>
2023-06-25 18:00:24 +0300

    add some blocks

--- src/js/main.js
+++ src/js/main.js
@@ -40,3 +40,38 @@ function fixedNav() {
   }
 }
 window.addEventListener('scroll', fixedNav)
+
+
+// Swiper
+const swiper = new Swiper('.swiper__testimonials', {
+  
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.next__btn',
+    prevEl: '.prev__btn',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+
+//   // Responsive breakpoints
+//   breakpoints: {
+//   // when window width is >= 320px
+//   320: {
+//     slidesPerView: 3,
+//     spaceBetween: 20
+//   },
+//   // when window width is >= 480px
+//   480: {
+//     slidesPerView: 3,
+//     spaceBetween: 30
+//   },
+// }
+});
